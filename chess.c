0b9d8b32f9806433f2d0a2a5c5f8d9482e870b0d
HashDG <hashdg2002@gmail.com>
2025-04-11 19:41:11 +0200

    Début gestions tours

--- chess.c
+++ chess.c
@@ -20,6 +20,7 @@ char recuperer(int);
 int pos(char*);
 int traiter_coup(char*);
 int traiter_coup_pion(char*);
+int traiter_coup_tour(char*);
 int placer(char, int);
 int retirer(char, int);
 bool existe(bitboard, int);
@@ -130,7 +131,12 @@ int traiter_coup(char* coup) {
 	int resultat = 0;
 	
 	// prise par un pion
-	if (1) {
+	if (coup[0] == TOUR) {
+		coup++;
+		if (pos(coup) < 0 && coup[1] != 'x') return ERR_FORMAT;
+		resultat = traiter_coup_tour(coup);
+	} else {
+		if (pos(coup) < 0 && coup[1] != 'x') return ERR_FORMAT;
 		resultat = traiter_coup_pion(coup);
 	}
 	
@@ -141,8 +147,6 @@ int traiter_coup(char* coup) {
  * Si le coup est valide, la fonction renvoie la position de la case à l'initiative
  */
 int traiter_coup_pion(char* coup) {
-	if (pos(coup) < 0 && coup[1] != 'x') return ERR_FORMAT;
-
 	char origine[3] = {0}, temp[3] = {0};
 	bitboard pion, npion;
 	
@@ -236,6 +240,107 @@ int traiter_coup_pion(char* coup) {
 	}
 	
 	return REUSSITE; 
+}
+
+int traiter_coup_tour(char* coup) {
+	char origine[3] = {0}, temp[3] = {0};
+	bitboard pion, npion;
+	
+	if (coup[0] == 'x') { /* Txe4 */
+		printf("destination : %s\n", coup + 2);
+		/* si la destination est un pion de la couleur courante erreur */
+		if (existe(COTE_COURANT, pos(coup + 2))) return ERR_DESTINATION;
+		/* si la destination n'est pas de l'autre couleur erreur*/
+		if (!existe(AUTRE_COTE, pos(coup + 2))) return ERR_DESTINATION;
+		
+		origine[0] = coup[0];
+		origine[1] = coup[3] - (trait_aux_blancs ? 1 : -1);
+		
+		printf("origine : %s\n", origine);
+		
+		/* si l'origine n'est pas de la couleur courante erreur*/
+		if (!existe(COTE_COURANT, pos(origine))) return ERR_ORIGINE;
+		
+		pion = (1ULL << pos(origine));
+		
+		if (trait_aux_blancs) {
+			npion = pion << (8 + (coup[2] - coup[0]));
+		} else {
+			npion = pion >> (8 + (coup[0] - coup[2]));
+		}
+		
+		/* On met à jour le déplacement du pion, puis on force l'inversion des plateaux étrangers*/
+		if (trait_aux_blancs) {		
+			pb = (pb & ~pion) | npion;
+			pn &= ~pb;
+			cn &= ~pb;
+			fn &= ~pb;
+			tn &= ~pb;
+			dn &= ~pb;
+		} else {
+			pn = (pn & ~pion) | npion;
+			pb &= ~pn;
+			cb &= ~pn;
+			fb &= ~pn;
+			tb &= ~pn;
+			db &= ~pn;
+		}
+	} else if (coup[1] == 'x') { /* Taxd4 */
+	
+	
+	
+	} else if (coup[1] >= 'a' && coup[1] <= 'h') /* Tad4 */
+	
+	} else { /* Td4 */
+		if (existe(BLANCS | NOIRS, pos(coup))) return ERR_DESTINATION;
+		
+		origine[0] = coup[0];
+		temp[0] = coup[0];
+		if (coup[1] ==  (trait_aux_blancs ? '4' : '5')) {
+			origine[1] = coup[1] - 2 * (trait_aux_blancs ? 1 : -1);
+			temp[1] = coup[1] - (trait_aux_blancs ? 1 : -1);
+			printf("origine: %s, temp: %s\n", origine, temp);
+			if (!existe(COTE_COURANT, pos(origine))
+				|| existe(COTE_COURANT, pos(temp))) {
+				origine[1] = temp[1];
+				
+				pion = (1ULL << pos(origine));
+				if (trait_aux_blancs) {
+					npion = pion << 8;
+				} else {
+					npion = pion >> 8;
+				}
+			} else {
+				pion = (1ULL << pos(origine));
+				if (trait_aux_blancs) {
+					npion = pion << 16;
+				} else {
+					npion = pion >> 16;
+				}
+			}
+		} else {
+			origine[1] = coup[1] - (trait_aux_blancs ? 1 : -1);
+			pion = (1ULL << pos(origine));
+			if (trait_aux_blancs) {
+				npion = pion << 8;
+			} else {
+				npion = pion >> 8;
+			}
+		}
+		
+		printf("origine : %s, %c\n", origine, recuperer(pos(origine)));
+		if (!existe(COTE_COURANT, pos(origine))) return ERR_ORIGINE;
+		
+		
+		/* On met à jour le déplacement du pion, puis on force l'inversion des plateaux étrangers*/
+		if (trait_aux_blancs) {
+			pb = (pb & ~pion) | npion;
+		} else {
+			pn = (pn & ~pion) | npion;
+		}
+	}
+	
+	return REUSSITE;
 }
 
 int afficher(void) {
